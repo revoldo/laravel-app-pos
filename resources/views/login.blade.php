3df8497067fc298cccee4092a7ea2d9d8cd08797
revoldo <revirinaldi@gmail.com>
2019-04-13 00:01:33 +0700

    mulsi

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,5 +4,38 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		
+
+		<title>Log in</title>
+		<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+
+		<link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+
+		<link rel="stylesheet" href="{{asset('css/font-awesome.min.css')}}">
+
+		<link rel="stylesheet" href="{{asset('css/ionicons.min.css')}}">
+
+		<link rel="stylesheet" href="{{asset('css/AdminLTE.min.css')}}">
+
+		<link rel="stylesheet" href="{{asset('css/blue.css')}}">
+
+		 <link rel="stylesheet"
+    href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+
 	</head>
+
+	<body class="hold-transition login-page">
+		<div class="login-box">
+			<div class="login-logo">
+				<img src="{{asset('/img/RevoldosLogo.png')}}" width="200" alt="">
+			</div>
+
+			<div class="login-box-body">
+				<p class="login-box-msg">Login untuk menggunakan aplikasi</p>
+
+
+				
+			</div>
+		</div>
+		
+	</body>
 </html>
